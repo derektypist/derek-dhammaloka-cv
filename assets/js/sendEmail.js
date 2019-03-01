70f8bfb33552c14c12c1a0f56e249aec68ae1a6b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-03-01 12:40:53 +0000

    Added sendEmail.js

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    
+    emailjs.send("outlook", "derek", { 
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_summary": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    
+    );
+        return false; // To block from loading a new page
+
+}
