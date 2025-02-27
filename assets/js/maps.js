1597556df111cd8c069bfe411a9ed8486a3d1677
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2025-02-27 16:20:15 +0000

    Added building markers at maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,141 +1,3 @@
-function initMap() {
-
-    var map = new google.maps.Map(document.getElementById("map"), {
-
-        zoom: 3,
-
-        center: {
-
-            lat: 46.619261,
-
-            lng: -33.134766
-
-        }
-
-    });
-
-
-
-    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-
-    /*
-    Label A is Cambridge Science Park
-    Label B is University of Leicester,
-    Label C is Best Western Hotel Acocks Green (Birmingham)
-    Label D is IoD Pall Mall London
-    Label E is Copthorne Hotel Merry Hill Dudley
-    Label F is Hotel Novotel London West (Hammersmith)
-    Label G is Copthorne Tara Hotel London Kensington
-    Label H is De Morgan House (Conference Centre) in Russell Square, London
-    Label I is Raspberry PI Foundation Cambridge
-    Label J is Hot Numbers Cambridge
-    Label K is Cambridge Brew House
-    Label L is The Maypole Cambridge
-    Label M is The Brook Cambridge
-    Label N is Grain and Hop Store Cambridge
-    Label O is Chesterton Mill Cambridge
-    */
-
-    var locations = [{
-
-        lat: 52.2320998,
-
-        lng: 0.1415806
-
-    }, {
-
-        lat: 52.6211393,
-
-        lng: -1.1246325
-
-    }, {
-
-        lat: 52.4455676,
-
-        lng: -1.8271322
-
-    }, {
-
-        lat: 51.5071851,
-
-        lng: -0.1316683
-
-
-    }, {
-
-        lat: 52.4850317,
-
-        lng: -2.1150972
-
-    }, {
-
-        lat: 51.4920396,
-
-        lng: -0.2198412
-
-    }, {
-
-        lat: 51.4985245,
-
-        lng: -0.1908758
-
-
-    }, {
-
-        lat: 51.521232,
-
-        lng: -0.124308
-
-    },
-
-    {
-        lat: 52.1966209,
-        lng: 0.1307118
-    },
-
-    {
-        lat: 52.2000961,
-        lng: 0.1384987
-    },
-    {
-        lat: 52.20744,
-        lng: 0.122225
-    }, {
-        lat: 52.2096674,
-        lng: 0.1192446
-    }, {
-        lat: 52.1961124,
-        lng: 0.155374
-    }, {
-        lat: 52.2188359,
-        lng: 0.1139751
-    }, {
-        lat: 52.2006212,
-        lng: 0.126302
-    }];
-
-    var markers = locations.map(function (location, i) {
-
-        return new google.maps.Marker({
-
-            position: location,
-
-            label: labels[i % labels.length]
-
-        });
-
-    });
-
-
-
-    var markerCluster = new MarkerClusterer(map, markers, {
-
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-
-    });
-
-}
-
 // Set Up Map
 
 const streets = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -173,21 +35,50 @@ const buildingIcon = L.extraMarkers.icon({
 let buildingCoords = [];
 
 let buildingLocations = [[52.2320998, 0.1415806,
-    "Cambridge Science Park"], [52.6211393, -1.1246325, "University of Leicester"
+    "Cambridge Science Park","BCS Event in June 2012"], [52.6211393, -1.1246325, "University of Leicester"
 
-], [52.4455676, -1.8271322, "Best Western Hotel Acocks Green (Birmingham)"],
-[51.5071851, -0.1316683, "IoD Pall Mall London"],
-[52.4850317, -2.1150972, "Copthorne Hotel Merry Hill Dudley"],
-[51.4920396, -0.2198412, "Novotel Hotel London West Hammersmith"],
-[51.4985245, -0.1908758, "Copthorne Tara Hotel London Kensington"],
-[51.521232, -0.124308, "De Morgan House (Conference Centre) Russell Square, London"],
-[52.1966209, 0.1307118, "Raspberry Pi Foundation"],
-[52.2000961, 0.1384987, "Hot Numbers Cambridge"],
-[52.20744, 0.122225, "The Cambridge Brew House"],
-[52.2096674, 0.1192446, "The Maypole Cambridge"],
-[52.1961124, 0.155374, "The Brook Cambridge"],
-[52.2188359, 0.1139751, "The Hop and Grain Store Cambridge"],
-[52.2006212, 0.126302, "Chesterton Mill Cambridge"]
+], [52.4455676, -1.8271322, "Best Western Hotel Acocks Green (Birmingham)","BCS Event in Feburary 2011"],
+[51.5071851, -0.1316683, "IoD Pall Mall London","BCS Annual Dinner in September 2011"],
+[52.4850317, -2.1150972, "Copthorne Hotel Merry Hill Dudley","Ideal for Conferences"],
+[51.4920396, -0.2198412, "Novotel Hotel London West Hammersmith","Ideal for Conferences"],
+[51.4985245, -0.1908758, "Copthorne Tara Hotel London Kensington","Ideal for Conferences"],
+[51.521232, -0.124308, "De Morgan House (Conference Centre) Russell Square, London","IMA Event in 2005"],
+[52.1966209, 0.1307118, "Raspberry Pi Foundation","Ideal for Meetups"],
+[52.2000961, 0.1384987, "Hot Numbers Cambridge","Ideal for Meetups"],
+[52.20744, 0.122225, "The Cambridge Brew House","Ideal for Meetups"],
+[52.2096674, 0.1192446, "The Maypole Cambridge","Ideal for Meetups"],
+[52.1961124, 0.155374, "The Brook Cambridge","Ideal for Meetups"],
+[52.2188359, 0.1139751, "The Hop and Grain Store Cambridge","Ideal for Meetups"],
+[52.2006212, 0.126302, "Chesterton Mill Cambridge","Ideal for Meetups"]
 
 ];
 
+for (let i=0;i<buildingLocations.length; i++) {
+    let buildingArticleText = '';
+    let arr = buildingLocations[i];
+    let title = arr[2];
+    let purpose = arr[3];
+    let latitude = arr[0];
+    let longitude = arr[1];
+
+    // Build the Text
+    buildingArticleText += `<h4>${title}</h4>`;
+    buildingArticleText += `<p>${purpose}</p>`;
+
+    // Add Latitude, Longitude, Title and Text to buildingCoords array
+    buildingCoords.push([latitude, longitude, title, buildingArticleText]);
+
+}
+
+// Plot the Buildings
+
+for (let i=0; i<buildingCoords.length; i++) {
+    let arr = buildingCoords[i];
+    let title = arr[2];
+    let description = arr[3];
+    let marker = L.marker(new L.LatLng(arr[0], arr[1]), {icon: buildingIcon, title: title});
+    marker.bindPopup(description);
+    buildingMarkers.addLayer(marker);
+}
+
+map.addLayer(buildingMarkers);
