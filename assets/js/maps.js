9cfce5012d55b7ebb60ab95a9d3c6f78a8175ef8
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2025-02-28 13:15:42 +0000

    Updated links

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -39,27 +39,27 @@ const buildingIcon = L.ExtraMarkers.icon({
 let buildingCoords = [];
 
 let buildingLocations = [[52.2320998, 0.1415806,
-    "Cambridge Science Park",{description: "BCS Event - Cambridge Data Mash Up June 2012", url:"https://www.cambridgesciencepark.co.uk/"}], [52.6211393, -1.1246325, "University of Leicester",{description:"BCS Events between 2005 and 2015.",url:"https://le.ac.uk/"}
+    "Cambridge Science Park", { description: "BCS Event - Cambridge Data Mash Up June 2012", url: "https://www.cambridgesciencepark.co.uk/" }], [52.6211393, -1.1246325, "University of Leicester", { description: "BCS Events between 2005 and 2015.", url: "https://le.ac.uk/" }
 
-], [52.4455676, -1.8271322, "Best Western Hotel Acocks Green (Birmingham)",{description:"BCS Event in Feburary 2011",url:""}],
-[51.5071851, -0.1316683, "IoD Pall Mall London",{description:"BCS Annual Dinner in September 2011",url:""}],
-[52.4850317, -2.1150972, "Copthorne Hotel Merry Hill Dudley",{description:"IQPS National Conference in October 2004.",url:""}],
-[51.4920396, -0.2198412, "Novotel Hotel London West Hammersmith",{description:"IQPS Spring Conference on Vision Without Boundaries in April 2002.",url:""}],
-[51.4985245, -0.1908758, "Copthorne Tara Hotel London Kensington",{description:"IQPS Spring Conference in Spring 2004.",url:""}],
-[51.521232, -0.124308, "De Morgan House (Conference Centre) Russell Square, London",{description:"IMA Event in April 2005",url:"https://www.demorganhouse.org.uk/"}],
-[52.1966209, 0.1307118, "Raspberry Pi Foundation",{description:"Meetups with Cambridge Python User Group between December 2019 and February 2020.",url:"https://www.raspberrypi.org/"}],
-[52.2000961, 0.1384987, "Hot Numbers Cambridge",{description:"Ideal for Meetups",url:"https://hotnumberscoffee.co.uk/gwydir-st/"}],
-[52.20744, 0.122225, "The Cambridge Brew House",{description:"Ideal for Meetups",url:""}],
-[52.2096674, 0.1192446, "The Maypole Cambridge",{description:"Meetup Event ",url:""}],
-[52.1961124, 0.155374, "The Brook Cambridge",{description:"Meetups with Cambridge Singles between January 2023 and February 2023.",url:"https://thebrookpub.co.uk/"}],
-[52.2188359, 0.1139751, "The Hop and Grain Store Cambridge",{description:"Ideal for Meetups",url:"https://www.greeneking.co.uk/pubs/cambridgeshire/grain-and-hop-store"}],
-[52.2006212, 0.126302, "Chesterton Mill Cambridge",{description:"Meetup Event with JavaScript & NodeJS Cambridge in November 2023.",url:"https://www.chestertonmill.com/"}],
-[52.239520, 0.278297, "Black Horse Inn Swaffham Bulbeck",{description:"Meetup Event on Valentines Dinner Date Ages 40+ in February 2014.",url:"https://blackhorseinnswaffham.co.uk/"}],
-[53.347690, -2.880654, "Crowne Plaza Liverpool John Lennon Airport",{description:"National Conference with IQPS on Come Fly with Us in October 2002.",url:"https://www.ihg.com/crowneplaza/hotels/gb/en/liverpool/lplcp/hoteldetail"}]
+], [52.4455676, -1.8271322, "Best Western Hotel Acocks Green (Birmingham)", { description: "BCS Event in Feburary 2011", url: "" }],
+[51.5071851, -0.1316683, "IoD Pall Mall London", { description: "BCS Annual Dinner in September 2011", url: "" }],
+[52.4850317, -2.1150972, "Copthorne Hotel Merry Hill Dudley", { description: "IQPS National Conference in October 2004.", url: "" }],
+[51.4920396, -0.2198412, "Novotel Hotel London West Hammersmith", { description: "IQPS Spring Conference on Vision Without Boundaries in April 2002.", url: "https://all.accor.com/hotel/0737/index.en.shtml" }],
+[51.4985245, -0.1908758, "Copthorne Tara Hotel London Kensington", { description: "IQPS Spring Conference in Spring 2004.", url: "" }],
+[51.521232, -0.124308, "De Morgan House (Conference Centre) Russell Square, London", { description: "IMA Event in April 2005", url: "https://www.demorganhouse.org.uk/" }],
+[52.1966209, 0.1307118, "Raspberry Pi Foundation", { description: "Meetups with Cambridge Python User Group between December 2019 and February 2020.", url: "https://www.raspberrypi.org/" }],
+[52.2000961, 0.1384987, "Hot Numbers Cambridge", { description: "Meetup Event with JavaScript & NodeJS Cambridge in January 2020.", url: "https://hotnumberscoffee.co.uk/gwydir-st/" }],
+[52.20744, 0.122225, "The Cambridge Brew House", { description: "Ideal for Meetups", url: "" }],
+[52.2096674, 0.1192446, "The Maypole Cambridge", { description: "Meetup Event with JavaScript & NodeJS Cambridge in January 2023.", url: "http://maypolefreehouse.co.uk/" }],
+[52.1961124, 0.155374, "The Brook Cambridge", { description: "Meetups with Cambridge Singles between January 2023 and February 2023.", url: "https://thebrookpub.co.uk/" }],
+[52.2188359, 0.1139751, "The Hop and Grain Store Cambridge", { description: "Meetup Event with JavaScript & NodeJS Cambridge in January 2024.", url: "https://www.greeneking.co.uk/pubs/cambridgeshire/grain-and-hop-store" }],
+[52.2006212, 0.126302, "Chesterton Mill Cambridge", { description: "Meetup Event with JavaScript & NodeJS Cambridge in November 2023.", url: "https://www.chestertonmill.com/" }],
+[52.239520, 0.278297, "Black Horse Inn Swaffham Bulbeck", { description: "Meetup Event on Valentines Dinner Date Ages 40+ in February 2014.", url: "https://blackhorseinnswaffham.co.uk/" }],
+[53.347690, -2.880654, "Crowne Plaza Liverpool John Lennon Airport", { description: "National Conference with IQPS on Come Fly with Us in October 2002.", url: "https://www.ihg.com/crowneplaza/hotels/gb/en/liverpool/lplcp/hoteldetail" }]
 
 ];
 
-for (let i=0;i<buildingLocations.length; i++) {
+for (let i = 0; i < buildingLocations.length; i++) {
     let buildingArticleText = '';
     let arr = buildingLocations[i];
     let title = arr[2];
@@ -80,11 +80,11 @@ for (let i=0;i<buildingLocations.length; i++) {
 
 // Plot the Buildings
 
-for (let i=0; i<buildingCoords.length; i++) {
+for (let i = 0; i < buildingCoords.length; i++) {
     let arr = buildingCoords[i];
     let title = arr[2];
     let description = arr[3];
-    let marker = L.marker(new L.LatLng(arr[0], arr[1]), {icon: buildingIcon, title: title});
+    let marker = L.marker(new L.LatLng(arr[0], arr[1]), { icon: buildingIcon, title: title });
     marker.bindPopup(description);
     buildingMarkers.addLayer(marker);
 }
