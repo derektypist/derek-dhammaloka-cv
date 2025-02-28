2c81d49aa7524b194a946e2dc285b53ad06f475d
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2025-02-28 12:07:44 +0000

    Added satellite at maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -5,9 +5,13 @@ const streets = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
+const satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
 let map = L.map('map', { layers: [streets] }).fitWorld();
 
-const basemaps = { "Streets": streets };
+const basemaps = { "Streets": streets, "Satellite": satellite };
 const buildingMarkers = L.markerClusterGroup();
 const overlays = { "Buildings": buildingMarkers };
 const layerControl = L.control.layers(basemaps, overlays).addTo(map);
